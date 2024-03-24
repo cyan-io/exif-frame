9d10867fbd0ea9e4d5856ef20dce0694708144e5
Cyan <2376302102@qq.com>
2024-03-24 09:52:16 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+with open("requirements.txt", encoding="utf-8") as f:
+    install_requires=f.readlines()
 
 setup(
     name="exif-frame",
@@ -15,7 +17,7 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     entry_points={"console_scripts": ["frame = frame.frame:main"]},
-    install_requires=["PyYAML", "pillow"],
+    install_requires=install_requires,
     platforms="any",
     long_description=long_description,
     long_description_content_type='text/markdown',
